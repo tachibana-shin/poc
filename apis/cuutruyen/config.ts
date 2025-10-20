afce8f7d53482149e5ba7d6b827a5f209560c96d
Tachibana Shin <tachibshin@duck.com>
2025-10-21 01:04:16 +0700

    aternal math operator

--- apis/cuutruyen/config.ts
+++ apis/cuutruyen/config.ts
@@ -1,3 +1,3 @@
 const proxy = process.env.HTTP_PROXY
 
-export const baseUrl = `${proxy ? `${proxy}?url=` : ""}${process.env.TARGET_URL ?? "https://cuutruyen.net"}`
+export const baseUrl = `${proxy ? `${proxy}?url=` : ""}${process.env.TARGET_URL || "https://cuutruyen.net"}`
