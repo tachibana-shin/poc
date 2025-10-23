c59f9b8086a6b0a82e6a8748fa06a6ed27ff4e3b
Tachibana Shin <tachibshin@duck.com>
2025-10-23 15:39:33 +0700

    feat: Add support for raw HTTP proxy URL
    This commit adds support for a raw HTTP proxy URL, allowing users to
    specify a proxy without the need for additional formatting. This is done
    by adding a new input variable `http_proxy_curl` and using it to set the
    proxy in the `cuutruyen` API configuration.

--- apis/cuutruyen/config.ts
+++ apis/cuutruyen/config.ts
@@ -1,3 +1,3 @@
-const proxy = process.env.HTTP_PROXY
+const proxy = process.env.HTTP_PROXY_CURL
 
 export const baseUrl = `${proxy ? `${proxy}?url=` : ""}${process.env.TARGET_URL || "https://cuutruyen.net"}`
